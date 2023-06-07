1fb652857636836203b42bce639a93e468df5aa4
Menelisi <mgmafana@gmail.com>
2023-06-07 19:10:11 +0000

    Using recursion to print a new line after a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _puts_recursion - Prints a string, followed by a new line
+* @c: String to be printed
+* Return: nothing
+*/
+
+void _puts_recursion(char *c)
+{
+	if (*c == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	
+	_putchar(*c);
+	c++
+	_puts_recursion(c);
+}
